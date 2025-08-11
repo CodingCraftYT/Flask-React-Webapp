bcb9ad46c033314d664f777221184ce4546f8d40
CodingCraft <codingcraftyt@gmail.com>
2025-08-11 14:38:20 +0530

    Updating code

--- api/app.py
+++ api/app.py
@@ -7,7 +7,3 @@ CORS(app)
 @app.route("/api")
 def index():
     return jsonify({"message": "Flask API connected on Render!"})
-
-# Run locally
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
