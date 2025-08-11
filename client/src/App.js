c7e3988749920401bb471e8d3b2e7a29c9ef3c00
CodingCraft <codingcraftyt@gmail.com>
2025-08-11 14:45:47 +0530

    Updating code

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("Loading...");
 
   useEffect(() => {
-    fetch("/api")
+    fetch("https://flask-backend-95a7.onrender.com/api")
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(() => setMessage("⚠️ Failed to fetch backend"));
